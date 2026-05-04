f070f2dfc44b7f5263d4e6829dacbe5c39871785
Shreyakush06 <shreyakush@gmail.com>
2026-05-05 03:44:26 +0530

    removed secret properly

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def get_db():
         user     = os.getenv("DB_USER"),
         password = os.getenv("DB_PASSWORD"),
         database = os.getenv("DB_NAME"),
-        ssl_disabled = os.getenv("DB_USE_SSL", "false").lower() != "true"
+        ssl_ca    = "/etc/ssl/certs/ca-certificates.crt"
     )
 
 def error_page(message):
